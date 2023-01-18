38cda37fbf7badf13cbcb21e222c61ec63b96bf1
Mario Work FOS <trabajo.makroz@gmail.com>
2023-01-18 15:51:38 -0400

    datatable mejorado

--- pages/guests.tsx
+++ pages/guests.tsx
@@ -1,25 +1,23 @@
 import { Avatar } from "flowbite-react";
 import { useState } from "react";
 import DataCrud from "../src/components/DataCrud";
+import DataTable from "../src/components/DataTable";
 import useAuth from "../src/hooks/useAuth";
 import useAxios from "../src/hooks/useAxios";
 import { getFields } from "../src/utils/dbTools";
-import { initialsName } from "../src/utils/string";
+import { capitalizeWords, initialsName } from "../src/utils/string";
 import styles from "../styles/guests.module.css";
 const guestPage = () => {
   const { user, config }: any = useAuth();
   const [params, setParams] = useState({
     page: 1,
     perPage: 10,
-    sortBy: "id",
+    sortBy: "name",
     orderBy: "asc",
     searchBy: "sponsor_id,=," + user.id,
     relations: "referidos",
   });
-  const { data, error, loaded, execute, reLoad } = useAxios("/members", "GET", {
-    ...params,
-    origen: "useAxios",
-  });
+  const { data } = useAxios("/members", "GET", params);
   const [formState, setFormState] = useState({});
   const [errorsForm, setErrorsForm] = useState({});
   const fields = getFields([
@@ -48,7 +46,7 @@ const guestPage = () => {
         className="flex-shrink-0"
       >
         <div className="space-y-1 font-medium dark:text-white">
-          <div>{row.name}</div>
+          <div>{capitalizeWords(row.name)}</div>
           <div className="text-sm text-gray-500 dark:text-gray-400">
             {row.icn}
           </div>
@@ -66,27 +64,44 @@ const guestPage = () => {
         <ol>
           {members.map((member: any, index: number) => {
             return (
-              <>
-                <li key={member.id + index}>
-                  <div className="rounded-full">
-                    <h2>
-                      <Avatar rounded>
-                        {member.name}
-                        <div className="text-sm">
-                          Dni: {member.icn}
-                          <br />
-                          Nivel: {member.level.title}
-                        </div>
-                      </Avatar>
-                    </h2>
-                  </div>
-                  {referido(member.referidos)}
-                </li>
-              </>
+              <li key={member.id + index}>
+                <div className="rounded-full">
+                  <h2>
+                    <Avatar rounded>
+                      {capitalizeWords(member.name)}
+                      <div className="text-sm">
+                        Dni: {member.icn}
+                        <br />
+                        Nivel: {member.level.title}
+                      </div>
+                    </Avatar>
+                  </h2>
+                </div>
+                {referido(member.referidos)}
+              </li>
             );
           })}
         </ol>
       </>
+    );
+  };
+
+  const onClickRowChildren = (row) => {
+    //console.log(row);
+    if (!row.referidos || row.referidos.length == 0) return "";
+
+    return (
+      <DataTable
+        datas={row.referidos}
+        columns={fields}
+        onClickRowChildren={onClickRowChildren}
+        params={{ ...params, total: data.total }}
+        onAction={null}
+        onChangePage={null}
+        onChangePerPage={null}
+        onChangeSort={null}
+        _sel={false}
+      />
     );
   };
   return (
@@ -131,12 +146,17 @@ const guestPage = () => {
       <DataCrud
         title="Invitado"
         modulo="members"
-        param={{ searchBy: "sponsor_id,=," + user.id }}
+        param={{
+          searchBy: "sponsor_id,=," + user.id,
+          relations: "referidos",
+          sortBy: "name",
+        }}
         columns={fields}
         formState={formState}
         setFormState={setFormState}
         errorsForm={errorsForm}
         setErrorsForm={setErrorsForm}
+        onClickRowChildren={onClickRowChildren}
       />
     </>
   );
