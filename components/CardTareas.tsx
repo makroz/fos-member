2213146715ca33181be3fe68cc8924e0f4c9ee6c
Mario Work FOS <trabajo.makroz@gmail.com>
2023-03-17 10:47:27 -0400

    flujo

--- components/CardTareas.tsx
+++ components/CardTareas.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { XCircle } from "react-feather";
+import { Clock, XCircle } from "react-feather";
 import Flecha from "./layout/Flecha";
 
 const CardTareas = ({ fecha, onClick, step, stepId }) => {
@@ -28,12 +28,16 @@ const CardTareas = ({ fecha, onClick, step, stepId }) => {
     "SABADO",
   ];
 
+  const [timer, setTimer] = useState(10);
+  const [end, setEnd] = useState(false);
+
   const click = () => {
     if (stepId == step && !open) {
       setOpen(true);
     } else {
       onClick(stepId);
       setOpen(false);
+      setEnd(false);
     }
   };
   useEffect(() => {
@@ -60,6 +64,25 @@ const CardTareas = ({ fecha, onClick, step, stepId }) => {
     hour: hour,
     ampm: d.getHours() >= 12 ? "PM" : "AM",
   };
+
+  useEffect(() => {
+    let interval: any;
+    if (open && step == stepId) {
+      interval = setInterval(() => {
+        setTimer((prev) => {
+          if (prev > 0) {
+            return prev - 1;
+          }
+          clearInterval(interval);
+          return 0;
+        });
+      }, 1000);
+    } else {
+      setTimer(10);
+    }
+    return () => clearInterval(interval);
+  }, [open]);
+
   return (
     <div
       className={
@@ -89,17 +112,13 @@ const CardTareas = ({ fecha, onClick, step, stepId }) => {
             (open ? "pb-2 pt-3" : "")
           }
         >
-          <div>{fecha2.lday}</div>
-          {/* {!open ? (
-            ""
-          ) : ( */}
+          <div>{end ? "FELICIDADES" : fecha2.lday}</div>
           <Flecha
             className={
               "transition-all duration-500 ease-in-out delay-500 " +
               (open ? "-translate-x-80" : "")
             }
           />
-
           <XCircle
             size={24}
             className={
@@ -107,13 +126,27 @@ const CardTareas = ({ fecha, onClick, step, stepId }) => {
             }
             onClick={(e) => setOpen(false)}
           />
-
-          {/* )} */}
         </div>
         <div>
           <div
             className={"relative bg-slate-600 " + (open ? "h-44 mb-6" : "h-0")}
           >
+            {end ? (
+              <div className=" flex justify-around items-center h-44 bg-gray-900">
+                <img
+                  src="/assets/images/star2.png"
+                  className="animate-bounce"
+                />
+                <img src="/assets/images/star1.png" className="animate-ping" />
+                <img
+                  src="/assets/images/star2.png"
+                  className="animate-bounce"
+                />
+              </div>
+            ) : (
+              ""
+            )}
+
             <div
               className={
                 "absolute -bottom-3 text-primary rotate-180 scale-[2.5] transition-all duration-500 ease-in-out  " +
@@ -130,36 +163,91 @@ const CardTareas = ({ fecha, onClick, step, stepId }) => {
             (open ? "h-28 scale-125 mb-4" : "h-20")
           }
         >
-          <div className="flex flex-col justify-center items-center w-1/2 border-r my-2 border-primary ">
-            <div
-              className={"text-[40px] text-secondary "}
-              style={{
-                lineHeight: "38px",
-                textShadow:
-                  "#fff 0px 0px 1px,   #fff 0px 0px 1px,   #fff 0px 0px 1px,#fff 0px 0px 1px,   #fff 0px 0px 1px,   #fff 0px 0px 1px",
-              }}
-            >
-              {fecha2.day}
+          {end ? (
+            <div className="flex flex-col justify-center items-center w-full  my-2  ">
+              <div
+                className={"text-[40px] text-secondary "}
+                style={{
+                  lineHeight: "38px",
+                  textShadow:
+                    "#fff 0px 0px 1px,   #fff 0px 0px 1px,   #fff 0px 0px 1px,#fff 0px 0px 1px,   #fff 0px 0px 1px,   #fff 0px 0px 1px",
+                }}
+              >
+                RETO
+              </div>
+              <div className="text-lg text-primary">CUMPLIDO</div>
             </div>
-            <div className="text-sm text-primary">{fecha2.lmonth}</div>
-          </div>
-          <div className="flex flex-col justify-center items-center w-1/2">
-            <div
-              className="text-[40px] text-secondary"
-              style={{
-                lineHeight: "38px",
-                textShadow:
-                  "#fff 0px 0px 1px,   #fff 0px 0px 1px,   #fff 0px 0px 1px,#fff 0px 0px 1px,   #fff 0px 0px 1px,   #fff 0px 0px 1px",
-              }}
-            >
-              {fecha2.hour}
-            </div>
-            <div className="text-sm text-primary">{fecha2.ampm}</div>
-          </div>
+          ) : (
+            <>
+              <div className="flex flex-col justify-center items-center w-1/2 border-r my-2 border-primary ">
+                <div
+                  className={"text-[40px] text-secondary "}
+                  style={{
+                    lineHeight: "38px",
+                    textShadow:
+                      "#fff 0px 0px 1px,   #fff 0px 0px 1px,   #fff 0px 0px 1px,#fff 0px 0px 1px,   #fff 0px 0px 1px,   #fff 0px 0px 1px",
+                  }}
+                >
+                  {fecha2.day}
+                </div>
+                <div className="text-sm text-primary">{fecha2.lmonth}</div>
+              </div>
+              <div className="flex flex-col justify-center items-center w-1/2">
+                <div
+                  className="text-[40px] text-secondary"
+                  style={{
+                    lineHeight: "38px",
+                    textShadow:
+                      "#fff 0px 0px 1px,   #fff 0px 0px 1px,   #fff 0px 0px 1px,#fff 0px 0px 1px,   #fff 0px 0px 1px,   #fff 0px 0px 1px",
+                  }}
+                >
+                  {fecha2.hour}
+                </div>
+                <div className="text-sm text-primary">{fecha2.ampm}</div>
+              </div>
+            </>
+          )}
         </div>
-        <button className="btn mt-2 btn-primary z-10" onClick={(e) => click()}>
-          VER
-        </button>
+        {end && (
+          <button
+            className="btn mt-2 btn-primary z-10"
+            onClick={(e) => click()}
+          >
+            TERMINAR RETO
+          </button>
+        )}
+        {!end &&
+          (open ? (
+            <button
+              className={
+                "btn mt-2  z-10 flex justify-around " +
+                (timer > 0 ? "bg-[#353336]" : "bg-primary")
+              }
+              // onClick={(e) => click()}
+            >
+              {timer > 0 ? (
+                <>
+                  <Clock size={24} />
+                  <span> 1D 00:00:{timer}</span>
+                </>
+              ) : (
+                <a
+                  href="https://meet.google.com/dki-buuu-zsz"
+                  target="_blank"
+                  onClick={(e) => setEnd(true)}
+                >
+                  ENTRAR al Challenge
+                </a>
+              )}
+            </button>
+          ) : (
+            <button
+              className="btn mt-2 btn-primary z-10"
+              onClick={(e) => click()}
+            >
+              VER
+            </button>
+          ))}
       </div>
     </div>
   );
